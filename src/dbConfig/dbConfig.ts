bc9e8235ee9b0b40116bdede1f7ccbeb46824461
Sayam Ghosh <sayamghosh.dev@gmail.com>
2024-11-21 00:27:48 +0530

    dbConfig created

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export async function connectDB(){
+    try {
+        await mongoose.connect(process.env.MONGO_URI!)
+        const connection = mongoose.connection;
+        connection.once('connected',()=>{console.log("Connected to the MongoDB")})
+        connection.on('error',(error)=>{console.log("Error connecting to the database",error)})
+        process.exit();
+        
+    } catch (error) {
+        console.log("Something went wrong while connecting to the database", error);
+    }
+}
