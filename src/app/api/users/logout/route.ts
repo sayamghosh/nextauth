f4a11d7e4a2aeeb2623fb76e91a207dead6cec59
Sayam Ghosh <sayamghosh.dev@gmail.com>
2024-11-21 22:24:37 +0530

    logout code written

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,6 +7,15 @@ import jwt from "jsonwebtoken";
 
 connectDB();
 
-export async function POST(request: NextRequest) {
-    
+export async function GET(request: NextRequest) {
+    try {
+       const response = NextResponse.json({
+        message: "Logout successful",
+        success: true,
+       })
+       response.cookies.set("token","",{httpOnly:true,expires:new Date(0)})  
+       return response;
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 }
