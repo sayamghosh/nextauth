a451071303d5e96edc7394d6ab174055468b3242
Sayam Ghosh <sayamghosh.dev@gmail.com>
2024-11-21 15:41:23 +0530

    bcrypt js installed

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,25 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/User";
+import { NextRequest,NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody=request.json()
+    const {username,email,password}=reqBody;
+    //validation
+    console.log(reqBody);
+
+    const user =await User.findOne({email});
+    if(user){
+      return NextResponse.json({error:"User already exists"},{status:400});
+    }
+
+
+  } catch (error:any) {
+    console.error(error);
+    return NextResponse.json({error:error.message},{status:500});
+    
+  }
+}
