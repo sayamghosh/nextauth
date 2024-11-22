dc569ac86600afa4f0ba6502e08bcffa72f10d28
Sayam Ghosh <sayamghosh.dev@gmail.com>
2024-11-22 09:46:52 +0530

    basic FE signup  setup done

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { set } from 'mongoose'
+import React, { useState } from 'react'
 
 function page() {
+
+  const[user,setUser]= useState({email:"", username:"", password:""}) 
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  const onSignup = async()=>{
+    try {
+      setLoading(true)
+      const response = await fetch("http://localhost:3000/api/users/signup",{
+        method:"POST",
+        body:formData,
+      } )
+    } catch (error:any) {
+      console.log("singup failed");
+    }
+  }
+
+
   return (
     <div>signup</div>
   )
